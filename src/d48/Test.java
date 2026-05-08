946c93d9f2ce01ed57c57a7e3d276ff3b0871342
itc-s25012 <s25012@std.it-college.ac.jp>
2026-05-08 12:35:54 +0900

    課題

---
+++ src/d48/Test.java
@@ -0,0 +1,7 @@
+package src.d48;
+
+public class Test {
+    public static void main(String[] args){
+        System.out.println("第48回");
+    }
+}
---
+++ src/d49/D49.java
@@ -0,0 +1,7 @@
+package src.d49;
+
+public class D49 {
+    public static void main(String[] args){
+        System.out.println("第49回");
+    }
+}
