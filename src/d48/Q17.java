cdb2447c90edc1d63eeafe4bb3c9f0db6ae9a532
itc-s25012 <s25012@std.it-college.ac.jp>
2026-05-15 10:38:37 +0900

    課題0515

---
+++ src/d48/Q17.java
@@ -0,0 +1,13 @@
+package src.d48;
+
+public class Q17 {
+    public static void main(String[] args){
+        int n = 35;
+        System.out.println(n > 15 || n <= 30);
+    }
+}
+
+/*
+&&演算子は比較演算子でandということ
+||演算子は比較演算子でorということ
+ */
