68d98f4c1d5c89e5b275125e73371482f3dd7af2
itc-s25012 <s25012@std.it-college.ac.jp>
2026-05-22 12:18:43 +0900

    課題0522

---
+++ src/d49/Q18.java
@@ -0,0 +1,11 @@
+package d49;
+
+public class Q18 {
+    public static void main(String[]ages){
+        int total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println((double) total/capacity * cargo);
+    }
+}
---
+++ src/d49/Q18Kadai.java
@@ -0,0 +1,11 @@
+package d49;
+
+public class Q18Kadai {
+    public static void main(String[] args){
+        int total = 75;
+        int capacity = 30;
+        int cargo = 2;
+
+        System.out.println((double) (total/capacity*cargo));
+    }
+}
