c94bdb98a48b114620f261aec549089e8ee72d75
itc-s25012 <s25012@std.it-college.ac.jp>
2026-05-22 12:00:58 +0900

    課題0522

---
+++ src/d49/Q17.java
@@ -0,0 +1,9 @@
+package d49;
+
+public class Q17 {
+    public static void main(String[] args){
+        int n = 1;
+
+        System.out.print(++n + "" + n--);
+    }
+}
---
+++ src/d49/Q17Kadai.java
@@ -0,0 +1,9 @@
+package d49;
+
+public class Q17Kadai {
+    public static void main(String[] args) {
+        int n = 1;
+
+        System.out.print(++n + "" + --n);
+    }
+}
