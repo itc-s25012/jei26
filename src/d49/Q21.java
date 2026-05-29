44c25880eea777338677bf03874086afbee9dab9
itc-s25012 <s25012@std.it-college.ac.jp>
2026-05-29 10:10:15 +0900

    課題0529

--- src/d49/Q21.java
+++ src/d49/Q21.java
@@ -2,8 +2,8 @@ package d49;
 
 public class Q21 {
     public static void main(String[]arg){
-        int n1 =010;
-        int n2 = 0x10;
+        int n1 =010; //010は10進数で"8"
+        int n2 = 0x10;  //0x10は16進数で"16"
 
         System.out.println(n1 + n2);
     }
