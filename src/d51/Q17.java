11c427c6c4e8a55502f1e6e1452c01e2f742a798
itc-s25012 <s25012@std.it-college.ac.jp>
2026-06-12 12:11:02 +0900

    課題0612

---
+++ src/d51/Q17.java
@@ -0,0 +1,12 @@
+package d51;
+
+public class Q17 {
+    public static void main(String[] args) {
+        int x = 4;
+        if (x > 4){
+            System.out.println("over 4");
+        } else if(x < 4){
+            System.out.println("under 4");
+        }
+    }
+}
---
+++ src/d51/Q17Kadai.java
@@ -0,0 +1,12 @@
+package d51;
+
+public class Q17Kadai {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x > 4){
+            System.out.println("over 4");
+        } else if(x < 4){
+            System.out.println("under 4");
+        }
+    }
+}
