1d82c81e3e06a505296639107822746307ff3a04
itc-s25012 <s25012@std.it-college.ac.jp>
2026-07-03 10:49:56 +0900

    課題0703

---
+++ src/d51/Q27.java
@@ -0,0 +1,9 @@
+package d51;
+enum Doneness {RARE, MEDIUM, WELLDONE}
+public class Q27 {
+    public static void main(String[] args) {
+        Doneness done =  Doneness.MEDIUM;
+        System.out.println(done);
+
+    }
+}
---
+++ src/d51/Q27Kadai.java
@@ -0,0 +1,8 @@
+package d51;
+enum Signal{Red, Yellow,Blue}
+public class Q27Kadai {
+    public static void main(String[] args) {
+        Signal done = Signal.Blue;
+        System.out.println(done);
+    }
+}
